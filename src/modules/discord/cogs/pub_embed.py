c991ba5043430cd2ad03f5160d1e5cf2b046db5b
J-Stuff <jstuff@j-stuff.net>
2024-11-05 14:08:23 +1300

    Nuke Maintenance mode

--- src/modules/discord/cogs/pub_embed.py
+++ src/modules/discord/cogs/pub_embed.py
@@ -18,7 +18,7 @@ logger = logging.getLogger("bot")
 class Functions:
     path = f"{Config.Paths.get_data_path()}/discord/database.json"
     # database schema
-    # {"channel":12346789, "message":123456789, "maintenance": False}
+    # {"channel":12346789, "message":123456789,}
 
     
     @classmethod
@@ -36,126 +36,11 @@ class Functions:
         with open(cls.path, 'w') as fp:
             json.dump(payload, fp)
 
-
-    @classmethod
-    def migrate_db(cls):
-        db = cls.read_discord_database()
-        if db == {}:
-            logger.debug("migrate attempted on empty database")
-            return
-        try:
-            db["maintenance"]
-        except KeyError:
-            db["maintenance"] = False
-            cls.write_discord_database(db)
-
-
 class PublicEmbed(commands.Cog):
     def __init__(self, bot:commands.Bot) -> None:
         self.bot = bot
         self.updateEmbed.start()
         super().__init__()
-
-    @app_commands.command(name="maintenance", description="Enables Maintenance mode on both the Public and Private Embeds")
-    @app_commands.dm_only()
-    @app_commands.describe(toggle="Enable or Disable Maintenance mode")
-    async def maintenance_mode(self, i:discord.Interaction, toggle: Literal["enabled", "disabled"]):
-        if i.user.id not in Config.DiscordUsers.authorized_users:
-            await i.response.send_message("You don't have permission to do this!", ephemeral=True, delete_after=30)
-            return
-        
-        if toggle == "enabled":
-            change_to = True
-        else:
-            change_to = False
-
-        db = Functions.read_discord_database()
-
-        if db == {}:
-            await i.response.send_message("Maintenance mode cannot be toggled because the bot has not been initialized yet!", ephemeral=True)
-            return
-        
-        channel_id = db["channel"]
-        message_id = db["message"]
-
-        if change_to:
-
-            db["maintenance"] = True
-            Functions.write_discord_database(db)
-
-            embed_channel = self.bot.get_channel(channel_id)
-
-            if type(embed_channel) != discord.TextChannel:
-                logger.exception("Embed Channel is not of type Text Channel")
-                await i.response.send_message("An Exception occurred while processing this request.", ephemeral=True)
-                raise Exception()
-
-            embed_message = await embed_channel.fetch_message(message_id)
-
-            maintenance_embed = discord.Embed(title="Maintenance", description="KISB is currently down for maintenance and will be back shortly...")
-            maintenance_embed.set_author(name="KISB", url="https://github.com/J-Stuff/KISB")
-
-            await embed_message.edit(content="", embed=maintenance_embed)
-            self.updateEmbed.stop()
-
-            await i.response.send_message("Done! Maintenance has been enabled.", ephemeral=True)
-
-        else:
-            db["maintenance"] = False
-            Functions.write_discord_database(db)
-            self.updateEmbed.start()
-            await i.response.send_message("Done, The maintenance embed should be replaced shortly.")
-
-            
-
-    
-    @app_commands.command(name="update-public-embed", description="Note: This disables Maintenance Mode")
-    @app_commands.dm_only()
-    @app_commands.describe(channel_id="The new channel ID you want to set")
-    async def update_embed_channel(self, i:discord.Interaction, channel_id:str):
-        if i.user.id not in Config.DiscordUsers.authorized_users:
-            await i.response.send_message("You don't have permission to do this!", ephemeral=True, delete_after=30)
-            return
-        
-        try:
-            channel = int(channel_id)
-        except:
-            await i.response.send_message("I could not parse the string you gave me into an int!", ephemeral=True)
-            return
-        
-        await i.response.defer(ephemeral=True, thinking=True)
-        
-        
-        try:
-            new_channel = await self.bot.fetch_channel(channel)
-        except discord.errors.NotFound:
-            await i.followup.send(f"I could not find a channel with that ID ({channel})")
-            return
-        except discord.errors.Forbidden:
-            await i.followup.send(f"I do not have access to that channel ({channel})")
-            return
-        except:
-            await i.followup.send("Something went wrong!", ephemeral=True)
-            return
-        
-        if type(new_channel) != discord.TextChannel:
-            await i.followup.send("You provided me a channel which is not of type `discord.TextChanel`", ephemeral=True)
-            return
-        
-        placeholder = await new_channel.send("<:Online:1196591854624444458>")
-
-
-        payload = {"channel": new_channel.id, "message": placeholder.id, "maintenance": False}
-
-        Functions.write_discord_database(payload=payload)
-
-        self.updateEmbed.start()
-
-        await i.followup.send("Done! The placeholder should update within 30 seconds.", ephemeral=True)
-        
-        
-
-
 
 
     @tasks.loop(seconds=15, reconnect=True)
@@ -166,22 +51,9 @@ class PublicEmbed(commands.Cog):
                 logger.info("KISB has not had its public embed initialized yet! I will stop the update loop until it is initialized.")
                 self.updateEmbed.stop()
                 return
-            
-
-            # Backwards Compatibility for pre-2.0.3
-            try:
-                db["maintenance"]
-            except KeyError:
-                Functions.migrate_db()
-                db = Functions.read_discord_database()
 
             channel_id = db["channel"]
             message_id = db["message"]
-            maintenance = db["maintenance"]
-            if maintenance:
-                logger.info("Maintenance mode enabled. Shutting down updateEmbed loop.")
-                self.updateEmbed.stop()
-                return
             
             channel = self.bot.get_channel(int(channel_id))
             message = await channel.fetch_message(int(message_id)) #type:ignore <- Type checking being an arse
